0d12312d70f72980f4120fb7fd7942eb2fe7b4d6
Marii2806 <107720808+Marii2806@users.noreply.github.com>
2024-03-31 22:05:00 +0000

    First commit, missing few steps

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -7,6 +7,10 @@ import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
+import { Login } from "./pages/login";
+import { Signup } from "./pages/signup";
+import { Private } from "./pages/private";
+
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
@@ -28,6 +32,9 @@ const Layout = () => {
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
+                        <Route element={<Signup />} path="/signup" />
+                        <Route element={<Login />} path="/login" />
+                        <Route element={<Private />} path="/private" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -46,7 +46,99 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
-			}
+			},
+
+			signupNewUser: async (formSignup)=>{
+				const url=process.env.BACKEND_URL
+				const signupRequirement="/api/signup"
+				try{
+					
+					const response= await fetch(url+signupRequirement,{
+						method:"POST",
+						body: JSON.stringify(formSignup),
+						headers:{
+							'Content-type': 'application/json'
+						},					
+					})
+	
+					if (response.ok){
+						const jsonResponse= await response.json()
+						console.log(jsonResponse)
+						const store = getStore()
+						setStore({...store,messageToShowAlert:jsonResponse})
+					}
+				
+					else{
+						const jsonResponse=await response.json()
+						console.log(jsonResponse)
+	
+					}
+	
+				}
+	
+				catch(e){
+					console.log("An error has occured",e)
+					
+				}
+			},
+			
+			loginUserExisting:async({ email, password })=>{
+				const url=process.env.BACKEND_URL;
+				const loginRequirement="/api/login"
+				try{
+					const response = await fetch(url+loginRequirement, {
+						method:'POST',
+						headers:{
+							'Content-type': 'application/json'
+						},
+						body:JSON.stringify({
+							email,
+							password
+						})
+					});
+					
+					if(response.status !==200) return false	
+						
+					const jsonResponse= await response.json()
+	
+					if (jsonResponse["token"]){
+						localStorage.setItem("userToken", jsonResponse["token"])
+						return true;
+	
+					}
+					return false;
+						
+				}
+			
+				catch(e){
+							console.log("An error is occurred",e)
+				}
+			},
+	
+			getInformationOfToken: async () => {
+				const url = process.env.BACKEND_URL;
+				const tokenRequirement = "/api/private";
+			 
+				try {
+					const response = await fetch(url + tokenRequirement, {
+						method: 'GET',
+						headers: {
+							'Authorization': `Bearer ${localStorage.getItem("userToken")}`
+						}
+					});
+			 
+					if (response.status !== 200) {
+						throw new Error(`Error: ${response.status}`);
+					}
+			 
+					const jsonResponse = await response.json();
+			 
+					return jsonResponse;
+
+				} catch (error) {
+					console.error("An error occurred: ", error);
+				}
+			 }
 		}
 	};
 };
